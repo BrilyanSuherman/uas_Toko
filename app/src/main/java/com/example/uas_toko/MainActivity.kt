16081c3f3cf04ebe82fbb99bd779279079302e9c
brilyan <brilyansuherman@gmail.com>
2023-07-16 14:44:37 +0700

    tambah aktivity chat

--- app/src/main/java/com/example/uas_toko/MainActivity.kt
+++ app/src/main/java/com/example/uas_toko/MainActivity.kt
@@ -2,11 +2,30 @@ package com.example.uas_toko
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.uas_toko.R
+import com.example.uas_toko.databinding.ActivityMainBinding
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
+    private lateinit var produkRecylerView : RecyclerView
+    private lateinit var db:FirebaseFirestore
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        produkRecylerView =binding.produkListView
+        produkRecylerView.layoutManager =LinearLayoutManager(this)
+        produkRecylerView.setHasFixedSize(true)
     }
+
+
+
+
 }
--- app/src/main/java/com/example/uas_toko/chat/Chat.kt
+++ app/src/main/java/com/example/uas_toko/chat/Chat.kt
@@ -1,3 +1,20 @@
 package com.example.uas_toko.chat
 
-data class Chat()
+import android.os.Message
+import com.google.firebase.firestore.Exclude
+
+data class Chat( val username: String? = null,
+                val message: String? = null,
+                val time: String? = null) {
+
+    @Exclude
+    fun getMap(): Map<String, Any?> {
+        return mapOf(
+            "username" to username,
+            "message" to message,
+            "time" to time
+        )
+    }
+}
+
+
